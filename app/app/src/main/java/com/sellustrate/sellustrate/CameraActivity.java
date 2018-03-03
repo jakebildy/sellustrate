8c16059a91c4cbf636918db457f1fa033c583e7d
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-03-03 09:02:15 -0800

    gives app permision to take photos

--- app/app/src/main/java/com/sellustrate/sellustrate/CameraActivity.java
+++ app/app/src/main/java/com/sellustrate/sellustrate/CameraActivity.java
@@ -2,12 +2,63 @@ package com.sellustrate.sellustrate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+static final int REQUEST_IMAGE_CAPTURE = 1;
+private File thisImage;
 public class CameraActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        dispatchTakePictureIntent();
+
     }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }
+            catch (IOException ex) {
+                // Error occurred while creating the File
+                System.out.println("image wasn't taken");
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg",storageDir);
+        thisImage=image;
+        return image;
+    }
+
+    public File getImage(){
+        return thisImage;
+    }
+
+
 }
