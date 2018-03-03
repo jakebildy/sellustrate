eb3854cacf5958ad512f2f559389a541a16a556d
Vivek Mishra <vivekmishra@Viveks-MacBook-Pro.local>
2018-03-03 14:53:37 -0800

    Added Ebay Files

---
+++ ebaystuff/common.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  3 10:55:25 2018
+
+@author: vivekmishra
+"""
+
+# -*- coding: utf-8 -*-
+'''
+© 2012-2013 eBay Software Foundation
+Authored by: Tim Keefer
+Licensed under CDDL 1.0
+'''
+
+def dump(api, full=False):
+
+    print("\n")
+
+    if api.warnings():
+        print("Warnings" + api.warnings())
+
+    if api.response.content:
+        print("Call Success: %s in length" % len(api.response.content))
+
+    print("Response code: %s" % api.response_code())
+    print("Response DOM1: %s" % api.response_dom()) # deprecated
+    print("Response ETREE: %s" % api.response.dom())
+
+    if full:
+        print(api.response.content)
+        print(api.response.json())
+        print("Response Reply: %s" % api.response.reply)
+    else:
+        dictstr = "%s" % api.response.dict()
+        print("Response dictionary: %s..." % dictstr[:150])
+        replystr = "%s" % api.response.reply
+        print("Response Reply: %s" % replystr[:150])
