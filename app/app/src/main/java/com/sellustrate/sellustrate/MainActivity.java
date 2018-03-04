3e240dabe749c7d8a1093bab9638f2c69fcd2c50
jakebildy <jbildy@uwo.ca>
2018-03-04 03:27:51 -0500

    adding sign-in

--- app/app/src/main/java/com/sellustrate/sellustrate/MainActivity.java
+++ app/app/src/main/java/com/sellustrate/sellustrate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sellustrate.sellustrate;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,8 +9,16 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.HttpURLConnection;
+import java.net.PasswordAuthentication;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -18,8 +27,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         super.onCreate(savedInstanceState);
 
-        Intent thingy = new Intent(this, LoadingActivity.class);
-          startActivity(thingy);
+        //Intent thingy = new Intent(this, LoadingActivity.class);
+         // startActivity(thingy);
 
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button2);
@@ -31,19 +40,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void onClick(View view)
     {
-        doSomething(view);
+        try {
+            doSomething(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void doSomething(View view) {
+    public void doSomething(View view) throws IOException {
         //The camera activity
         Intent cameraIntent = new Intent(this, CameraActivity.class);
 
         if (view.getId() == R.id.buttonNew)
             startActivity(cameraIntent);
         if (view.getId() == R.id.button2)
-            slide();
+            signIn();
+    }
 
+    public void signIn() throws IOException {
+        EditText userView = findViewById(R.id.username);
+        final String username = userView.getText().toString();
 
+        EditText passView = findViewById(R.id.password);
+        final String password = userView.getText().toString();
+
+        Authenticator.setDefault(new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username,password.toCharArray());
+            }
+        });
+        new HttpAsyncTask().execute("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=");
+        slide();
+    }
+
+    public class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            HttpURLConnection c = null;
+            try {
+                c = (HttpURLConnection) new URL("https://signin.ebay.com/ws/eBayISAPI.dll?SignIn&ru=").openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            c.setUseCaches(false);
+            try {
+                c.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println(c);
+            return "";
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
     }
 
     //slides everything to the right
