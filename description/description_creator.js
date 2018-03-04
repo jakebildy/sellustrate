bc021acdf1656dda46752e1da68dcd68f9f11ce7
jakebildy <jbildy@uwo.ca>
2018-03-04 04:36:47 -0500

    linking with python backend to enable keyword search via json

--- description/description_creator.js
+++ description/description_creator.js
@@ -1,13 +1,19 @@
 //Jake Bildy, 2018
 
+//Keyword to JSON object
+function key2JSON(keyword)
+{
+    var obj = {"search" : keyword};
+    return obj;
+}
 
 function getResponse(url)
 {
     var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("GET", url, false);
-    xmlHttp.send(null);
+    xmlHttp.send(key2JSON("bicycle"));
     return xmlHttp.responseText;
 }
 
-console.log(getResponse("http://sellustrate.azurewebsites.net"));
+console.log(getResponse("http://sellustrate.azurewebsites.net/desc"));
