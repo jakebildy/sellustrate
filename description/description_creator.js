e568f560101be042e907eed1ec6192a9f59b5205
jakebildy <jbildy@uwo.ca>
2018-03-04 05:25:14 -0500

    using request library

--- description/description_creator.js
+++ description/description_creator.js
@@ -1,19 +1,24 @@
 //Jake Bildy, 2018
 
-//Keyword to JSON object
-function key2JSON(keyword)
-{
-    var obj = {"search" : keyword};
-    return obj;
-}
+var request = require('request');
 
-function getResponse(url)
-{
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url, false);
-    xmlHttp.send(key2JSON("bicycle"));
-    return xmlHttp.responseText;
-}
+var JSONSearchObject = {"search" : "tshirt"};
 
-console.log(getResponse("http://sellustrate.azurewebsites.net/desc"));
+var responseJSON;
+
+request({
+    url: "http://sellustrate.azurewebsites.net/desc",
+    method: "POST",
+    json: true,
+    body: JSONSearchObject
+}, function (error, response, body){
+    console.log(response);
+});
+
+
+//descriptions = JSON.parse(getResponse("http://sellustrate.azurewebsites.net/desc"));
+
+//function getWordsFrequency()
+//{
+
+//}
