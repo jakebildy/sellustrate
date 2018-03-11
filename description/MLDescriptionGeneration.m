3a397e975078aa20537a7deb7e59ce25ed5b90bb
jakebildy <jbildy@uwo.ca>
2018-03-11 13:19:00 -0700

    implementing new functions

--- description/MLDescriptionGeneration.m
+++ description/MLDescriptionGeneration.m
@@ -10,14 +10,42 @@ generate(str);
 function y = generate(str)
 descriptions = split(lower(str), ",");
 
-descOne = descriptions(1);
-words = split(descOne, " ");
+test = getWordCommonalityTotal(descriptions)
+y = getWordMatrix(descriptions, 3)
+
+end
+
+function x = getWordMatrix(descriptions, val)
+
+desc = cleanUpSentence(descriptions(val));
+words = split(desc, " ");
 
 [~,wordID, count] = unique(words);
 
 n = accumarray(count,1);
 
-y = horzcat(num2cell(n), words(wordID)) %n occurences for each word
+x = horzcat(num2cell(n), words(wordID)); %n occurences for each word
+end
+
+function x = getWordCommonalityTotal(descriptions)
+
+
+str = cleanUpSentence(strjoin(descriptions));
+words = split(str, " ");
+
+[~,wordID, count] = unique(words);
+
+n = accumarray(count,1);
+
+x = flip(sortrows(horzcat(num2cell(n), words(wordID)),1)); %n occurences for each word
+end
+
+function x = cleanUpSentence(str)
+
+%Removes unwanted chars from descriptions
+x = regexprep(regexprep(regexprep(regexprep(strrep(strrep(str,':',''),'.',''),'\r',''),'\v',''),'\n',''),'"','');
+
 
 
 end
+
