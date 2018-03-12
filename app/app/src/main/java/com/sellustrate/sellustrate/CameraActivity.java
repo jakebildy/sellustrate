efa18f6ea6ce6f568de45ac8c4b24476c09a65fb
Agatha Turyahikayo <agathaturyahikayo14@gmail.com>
2018-03-11 21:54:01 -0400

    sends base 64 picture string to server

--- app/app/src/main/java/com/sellustrate/sellustrate/CameraActivity.java
+++ app/app/src/main/java/com/sellustrate/sellustrate/CameraActivity.java
@@ -1,6 +1,8 @@
 package com.sellustrate.sellustrate;
 
+import android.content.Intent;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -10,35 +12,32 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
-
-import org.apache.http.HttpEntity;
+import android.widget.Toast;
+import org.apache.commons.io.FileUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.FileEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONObject;
-
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static org.apache.commons.codec.binary.Base64.encodeBase64;
 
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     public static final String subscriptionKey = "cfa2ac95fcf04101b79b839837876d16";
-    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=Tags&language=en";
+    public static final String uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze";
     private Camera mCamera;
     private CameraPreview mCameraPreview;
 
-    private boolean safeToTakePicture = false;
+    public static String encodedString;
 
     /**
      * Called when the activity is first created.
@@ -49,33 +48,30 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         setContentView(R.layout.activity_camera);
         mCamera = getCameraInstance();
         mCameraPreview = new CameraPreview(this, mCamera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        FrameLayout preview =  findViewById(R.id.camera_preview);
         preview.addView(mCameraPreview);
-        safeToTakePicture = true;
-        Button captureButton = (Button) findViewById(R.id.button_capture);
+
+        Button captureButton = findViewById(R.id.button_capture);
         captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(safeToTakePicture) {
-                    mCamera.takePicture(null, null, mPicture);
-                    System.out.println("the picture was taken");
-                    safeToTakePicture=false;
-                }
+                mCamera.takePicture(null, null, mPicture);
 
+                startActivity(new Intent(CameraActivity.this, LoadingActivity.class));
             }
         });
     }
 
 
-    @RequiresApi(api = Build.VERSION_CODES.CUPCAKE)
     @Override
     public void onClick(View view) {
-
+        new HttpAsyncTask().execute("http://sellustrate.azurewebsites.net/cognition");
     }
 
-    /*
+    /**
      * Helper method to access the camera returns null if it cannot get the
      * camera or does not exist
+     *
      * @return
      */
     private Camera getCameraInstance() {
@@ -83,10 +79,11 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         try {
             camera = Camera.open();
         } catch (Exception e) {
-            System.out.println("this camera doesnt exist lol");
+            // cannot get camera or does not exist
         }
         return camera;
     }
+
     Camera.PictureCallback mPicture = new Camera.PictureCallback() {
         @RequiresApi(api = Build.VERSION_CODES.KITKAT)
         @Override
@@ -95,9 +92,26 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
 
             File pictureFile = getOutputMediaFile();
 
+//            try {
+//                analyzeImage(pictureFile.toString());
+//            } catch (URISyntaxException e) {
+//                e.printStackTrace();
+//            }
             System.out.println(pictureFile.toString() + "<----- file strig");
             if (pictureFile == null) {
                 return;
+            }
+            try {
+                byte[] encoded = new byte[0];
+                try {
+                    encoded = encodeBase64(FileUtils.readFileToByteArray(pictureFile));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                encodedString = new String(encoded, StandardCharsets.US_ASCII);
+                System.out.println(" <---- encoded string " + encodedString);
+
+            } catch (Exception e) {
             }
         }
     };
@@ -120,49 +134,64 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
         File mediaFile;
         mediaFile = new File(mediaStorageDir.getPath() + File.separator
                 + "IMG_" + timeStamp + ".jpg");
-        //JSONObject pictureTags =/
-        getPictureTags(mediaFile);
 
         return mediaFile;
     }
 
-
-    public static InputStream getPictureTags(File file) {
-
-     CloseableHttpClient httpclient = HttpClients.createDefault();
-
+    public static String post(String url, String encodedFile) {
+        InputStream inputStream = null;
+        String result = "";
         try {
-            URIBuilder uriBuilder = new URIBuilder(uriBase);
-           // uriBuilder.setParameter("returnFaceId", "true");
-           // uriBuilder.setParameter("returnFaceLandmarks", "false");
-           // uriBuilder.setParameter("returnFaceAttributes", "age");
+            HttpClient httpclient = new DefaultHttpClient();
 
-            URI uri = uriBuilder.build();
-            HttpPost request = new HttpPost(uri);
+            // make POST request to the given URL
+            HttpPost httpPost = new HttpPost(url);
 
-            request.setHeader("Content-Type", "application/json");
-            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            // set json to StringEntity
+            StringEntity se = new StringEntity(encodedFile);
 
+            // set httpPost Entity
+            httpPost.setEntity(se);
 
-            // Request body
-            FileEntity reqEntity = new FileEntity(file, ContentType.APPLICATION_JSON);
-            request.setEntity(reqEntity);
+            // Set some headers to inform server about the type of the content
+            // httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-type", "application/json");
 
-            HttpResponse response = httpclient.execute(request);
-            HttpEntity entity = response.getEntity();
-            System.out.println(response.getStatusLine());
+            // Execute POST request to the given URL
+            HttpResponse httpResponse = httpclient.execute(httpPost);
 
 
-            if (entity != null) {
-                System.out.println(EntityUtils.toString(entity));
-                return entity.getContent();
-            }
+            // receive response as inputStream
+            inputStream = httpResponse.getEntity().getContent();
+
+
+        } catch (Exception e) {
+            String errorMsg = e.getLocalizedMessage();
+            Log.d("InputStream", errorMsg);
+
         }
-        catch (Exception e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-        return null;
+        System.out.println(result);
+        // return result
+        return result;
     }
 
-}//end cameraActivity
+
+    private class HttpAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            return post(urls[0], encodedString);
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            Toast.makeText(getBaseContext(), "Data Sent!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+//end cameraActivity
+
+
+}
