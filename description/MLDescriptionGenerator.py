b3a7f3f818dd8d398b701e2c9ba0552c2ac6a55e
jakebildy <jbildy@uwo.ca>
2018-03-11 19:32:24 -0700

    converted old js to python

--- description/MLDescriptionGenerator.py
+++ description/MLDescriptionGenerator.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import random
 import networkx as nx
 
 fileID = open("descriptions.txt", "r")
@@ -39,7 +40,47 @@ def count_ratio(countsSmol, counts):
         return len(counts)/len(countsSmol)
 
 
-print(generate_descriptions(descriptions))
+qualityIntToDescription = {
+        0:
+        "The product is brand new and has never been opened.",
+        1:
+        "The product is brand new, but already opened. Barely touched.",
+        2:
+        "The product is brand new, but contains some defects. Still more than functional.",
+        3:
+        "The product is in good condition, having been recently refurbished by the manufacturer.",
+        4:
+        "The product is in good condition - recently refurbished.",
+        5:
+        "The product is in absolute mint condition.",
+        6:
+        "The product is in very good condition.",
+        7:
+        "The product is used, but still in very good condition.",
+        8:
+        "The product is quite well used, but still in an acceptable condition. Still works.",
+        9:
+        "The product is very heavily used, and no longer functions.",
+}
 
+
+randomComment = {
+        0:
+        " Definitely a great deal.",
+        1:
+        " Hurry now before it's too late!",
+        2:
+        " Going fast - make it yours today.",
+        3:
+        " You don't find a lot of deals like this.",
+        4:
+        " Definitely worth the price.",
+        5:
+        " Deals like this are hard to come by.",
+    }
+
+
+print(generate_descriptions(descriptions))
+print(qualityIntToDescription[random.randint(0, 9)]+randomComment[random.randint(0, 6)])
 
 
