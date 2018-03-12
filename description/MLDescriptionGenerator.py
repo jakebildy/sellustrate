1cb028ba04b465d3c00d2b61b138950e0b80b85e
jakebildy <jbildy@uwo.ca>
2018-03-11 17:25:39 -0700

    finished new functions

--- description/MLDescriptionGenerator.py
+++ description/MLDescriptionGenerator.py
@@ -1,20 +1,33 @@
+from collections import Counter
+
 fileID = open("descriptions.txt", "r")
 
 descriptions = fileID.read()
 
 
 def clean_up_sentence(sentence):
-    return sentence.replace('"', "").replace("\n", "").replace(".", "")
+    return sentence.replace('"', "").replace("\n", "").replace(".", "").replace(",", "").replace("  ", " ")
 
 
 def get_word_commonality_total(desc_array):
     desc = clean_up_sentence(" ".join(desc_array))
-    return desc
+    words = desc.split(" ")
+    counts = Counter(words)
+    return counts
+
+
+def get_word_commonality(sentences, val):
+    sentence = sentences[val]
+    desc = clean_up_sentence(sentence)
+    words = desc.split(" ")
+    counts = Counter(words)
+    return counts
 
 
 def generate_descriptions(desc):
-    desc_array = desc.lower().split()
+    desc_array = desc.lower().split(",")
     response = get_word_commonality_total(desc_array)
+    print(get_word_commonality(desc_array, 1))
     return response
 
 
