03754da32740e7f5c2cea64a41cb055fb9040b1c
jakebildy <jbildy@uwo.ca>
2018-06-12 22:21:51 -0400

    :boom: Creating listing preview as a WebView, converting MlDescriptionGenerator to JS

---
+++ listing/MLDescriptionGen.js
@@ -0,0 +1,44 @@
+//Replacing original code in python to make it Android compatible
+
+
+qualityIntToDescription = {
+    0:
+        "The product is brand new and has never been opened.",
+    1:
+        "The product is brand new, but already opened. Barely touched.",
+    2:
+        "The product is brand new, but contains some defects. Still more than functional.",
+    3:
+        "The product is in good condition, having been recently refurbished by the manufacturer.",
+    4:
+        "The product is in good condition - recently refurbished.",
+    5:
+        "The product is in absolute mint condition.",
+    6:
+        "The product is in very good condition.",
+    7:
+        "The product is used, but still in very good condition.",
+    8:
+        "The product is quite well used, but still in an acceptable condition. Still works.",
+    9:
+        "The product is very heavily used, and no longer functions.",
+}
+
+
+randomComment = {
+    0:
+        " Definitely a great deal.",
+    1:
+        " Hurry now before it's too late!",
+    2:
+        " Going fast - make it yours today.",
+    3:
+        " You don't find a lot of deals like this.",
+    4:
+        " Definitely worth the price.",
+    5:
+        " Deals like this are hard to come by.",
+}
+
+
+
